2287aa315ff11d3b449e0538aecdfce20b7bc486
@fuad2011 <M.Fuad.f2010@gmail.com>
2025-06-06 14:28:46 +0400

    чао

--- login.js
+++ login.js
@@ -1,19 +1,24 @@
+let num = +localStorage.getItem("num");
+
 let btn = document.getElementById("btn2")
 btn.addEventListener("click",()=>{
-    for(let i  = 0 ;i< b; i++){
+    for(let i  = 0 ;i< num; i++){
 let obj = JSON.parse(localStorage.getItem(`user${i}`))
 let i1 = document.getElementById("inp12")
 let i2 = document.getElementById("inp22")
-        if(i1.value == obj.name1 && i2.value == obj.password){
-            alert("ты зарегался харош")
-            location.href("index.html")
-            break;
-        }else if ((i1.value != obj.name1 && i2.value != obj.password)){
-            alert("чё то неправильно")
-        }
-        else{
-          continue;
-        }
+        if (i1.value == obj.username && i2.value == obj.password){
+            alert("ты зарегался харош");
+                window.location.href = "index.html";
+                if(i1.value == obj.username){
+                    localStorage.setItem("loginuser",JSON.stringify(obj))
+                }
+                break;
+            }else if ((i1.value != obj.username || i2.value != obj.password) && i == num-1) {
+                alert("неверный пароль или username");
+            }
+            else{
+                continue;
+            }
 }
     })
     
